33aff7efc8b7ff15ce770bfac2dd26ce173e1d53
Inna Kovtun <39884162+InnaKovtun@users.noreply.github.com>
2021-10-30 17:30:00 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,30 +1,4 @@
 $(document).ready(function(){
-    let menuBtn = $('.top-nav_btn');
-    let sidebarBtn = $('.left-sidebar_btn');
-    let menu = $('.top-nav_menu');
-    let sidebarMenu = $('.left-sidebar_menu');
-
-    menuBtn.on('click', function(event){
-        event.preventDefault();
-        menu.toggleClass('top-nav_menu__active');
-    });
-
-    sidebarBtn.on('click', function(event){
-        event.preventDefault();
-        sidebarMenu.toggleClass('left-sidebar_menu__active');
-    });
-
-    $('.gallery').slick({
-        arrows: true,
-        dots: true,
-        slidesToShow: 1,
-        slidesToScroll: 1
-    });
-
-
-});
-
-$(document).ready(function(){
     $('#event-carousel').carousel({
         indicators: false
     });
